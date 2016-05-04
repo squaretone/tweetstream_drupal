0a429494bfd5e3d58a6f2ac0afad3212b1d7eada
Eric Lawrence <eric@squaretone.com>
2016-05-04 11:03:24 -0500

    Use ng-src instead of src attribute to prevent preloading of placeholders

--- tweetstream.tpl.php
+++ tweetstream.tpl.php
@@ -8,14 +8,14 @@
   <div class="messages" ng-repeat="tweet in tweets">
     <div class="tweet media">
       <div class="media-left">
-        <img class="media-object" src="{{ tweet.profileImg }}" alt="profile image" />
+        <img class="media-object" ng-src="{{ tweet.profileImg }}" alt="profile image" />
       </div>
       <div class="media-body">
         <h5 class="media-heading">{{ tweet.name }} <small>{{ tweet.username }}</small></h5>
         {{ tweet.text }}
       </div>
       <div ng-repeat="item in tweet.media">
-        <img src="{{ item.url }}" width="{{ item.width }}" height="{{ item.height }}"/>
+        <img ng-src="{{ item.url }}" width="{{ item.width }}" height="{{ item.height }}"/>
       </div>
     </div>
   </div>
