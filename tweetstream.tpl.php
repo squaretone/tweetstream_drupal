68dd54ed54339c02934c8b573030851b66cf8451
Eric Lawrence <eric@squaretone.com>
2016-05-04 11:59:40 -0500

    Better stock css

--- tweetstream.tpl.php
+++ tweetstream.tpl.php
@@ -6,17 +6,18 @@
 ?>
 <div ng-controller="tweetStreamCtrl" class="tweetstream-default">
   <a class="pause-stream" ng-click="toggleStream()" ng-show="debugMode">{{ pauseText }}</a>
-  <div class="messages" ng-repeat="tweet in tweets">
-    <div class="tweet media">
-      <div class="media-left">
-        <img class="media-object" ng-src="{{ tweet.profileImg }}" alt="profile image" />
-      </div>
-      <div class="media-body">
-        <h5 class="media-heading">{{ tweet.name }} <small>{{ tweet.username }}</small></h5>
-        {{ tweet.text }}
-      </div>
-      <div ng-repeat="item in tweet.media">
-        <img ng-src="{{ item.url }}" width="{{ item.width }}" height="{{ item.height }}"/>
+  <div class="tweetstream-tweets" ng-repeat="tweet in tweets">
+    <div class="tweet-wrapper">
+      <div class="tweet media">
+        <div class="media-left twitter-profile-image">
+          <img class="media-object" ng-src="{{ tweet.profileImg }}" alt="profile image" />
+        </div>
+        <div class="media-body">
+          <div class="media-heading">{{ tweet.name }} <small>{{ tweet.username }}</small></div>
+          <div class="media-text">
+            {{ tweet.text }}
+          </div>
+        </div>
       </div>
     </div>
   </div>
